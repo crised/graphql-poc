9c64e9f187b3dff405e6d451ac12072e664a22df
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-09-20 12:19:15 -0300

    Added GraphQLError exception class

---
+++ src/main/java/com/udacity/graphql/controller/LocationController.java
@@ -0,0 +1,7 @@
+package com.udacity.graphql.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LocationController {
+}
---
+++ src/main/java/com/udacity/graphql/service/LocationServiceImpl.java
@@ -0,0 +1,7 @@
+package com.udacity.graphql.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LocationServiceImpl {
+}
