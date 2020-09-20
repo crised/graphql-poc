76909baf6b1d9fb683e2ab95715089d215643f0f
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-09-20 12:32:13 -0300

    Updated LocationController

--- src/main/java/com/udacity/graphql/controller/LocationController.java
+++ src/main/java/com/udacity/graphql/controller/LocationController.java
@@ -1,7 +1,27 @@
 package com.udacity.graphql.controller;
 
+import com.udacity.graphql.model.Location;
+import com.udacity.graphql.service.LocationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class LocationController {
+
+    private LocationService locationService;
+
+    public LocationController(LocationService locationService) {
+        this.locationService = locationService;
+    }
+
+    @GetMapping("/location")
+    public ResponseEntity<List<Location>> getAllLocations() {
+        List<Location> list = locationService.retrieveLocations();
+        return new ResponseEntity<List<Location>>(list, HttpStatus.OK);
+    }
 }
